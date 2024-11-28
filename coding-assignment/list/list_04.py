b82cd81de844d142dec75a1472cd1f563c46dcd3
priya-sharma-cs <priya.sharma.200530@gmail.com>
2024-11-28 09:05:18 +0530

    Create list_04.py

---
+++ coding-assignment/list/list_04.py
@@ -0,0 +1,4 @@
+''' reverse a list. Python provides several methods to reverse a list using built-in functions and manual approaches. The simplest way to reverse a list is by using the reverse() method.'''
+a=list(input())
+rev = list(reversed(a))
+print(rev)
