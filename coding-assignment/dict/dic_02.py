e6332219ce2c7a0d67663a2a78915cb03e249abe
priya-sharma-cs <priya.sharma.200530@gmail.com>
2024-11-28 20:03:04 +0530

    Create dic_02.py

---
+++ coding-assignment/dict/dic_02.py
@@ -0,0 +1,12 @@
+'''Write a Python script to add a key to a dictionary.'''
+# Create a dictionary 'd' with two key-value pairs.
+d = {0: 10, 1: 20}
+
+# Print the original dictionary 'd'.
+print(d)
+
+# Update the dictionary 'd' by adding a new key-value pair {2: 30}.
+d.update({2: 30})
+
+# Print the dictionary 'd' after the update, which now includes the new key-value pair.
+print(d) 
